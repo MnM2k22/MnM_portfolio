4a0ed3bcdbf107006fccab957002936b27a01809
MnM2k22 <115646994+MnM2k22@users.noreply.github.com>
2022-10-13 00:54:36 +0530

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -7,7 +7,7 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-        <a href='https://www.twitter.com'><BsTwitter /></a>
+        <a href='https://www.twitter.com/mnm_2k22'><BsTwitter /></a>
         </div>
         <div>
         <a href='https://www.facebook.com'><FaFacebook /></a>
@@ -19,10 +19,10 @@ const SocialMedia = () => {
         <a href='https://www.youtube.com/channel/UC90sDilVDnaN3idFsIHL0pQ'><FaYoutube /></a>
         </div>
         <div>
-        <a href='https://www.linkedin.com'><FaLinkedinIn /></a>
+        <a href='https://www.linkedin.com/in/mnm-team-788610253'><FaLinkedinIn /></a>
         </div>
         <div>
-        <a href='https://www.discord.com'><BsDiscord /></a>
+        <a href='https://discord.gg/kXvyQdAJ'><BsDiscord /></a>
         </div>
         <div>
         <a href='https://www.tiktok.com'><FaTiktok /></a>
