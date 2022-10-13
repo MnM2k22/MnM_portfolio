a824a6fa25783e7d2b2ecd2e5c5069692ea8a00e
MnM2k22 <115646994+MnM2k22@users.noreply.github.com>
2022-10-14 00:31:56 +0530

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,53 +1,53 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsDiscord, BsWhatsapp } from 'react-icons/bs'
-import {  FaFacebook, FaTiktok, FaYoutube, FaLinkedinIn, FaSpotify, FaWeibo, FaVk, FaVimeoV, FaTelegram, FaPatreon, FaMedium, FaPinterestP, FaTumblr } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsDiscord, BsWhatsapp } from 'react-icons/bs';
+import {  FaFacebook, FaYoutube, FaLinkedinIn, FaSpotify, FaVk, FaVimeoV, FaTelegram, FaPatreon, FaMedium, FaPinterestP, FaTumblr } from 'react-icons/fa';
+import { SiUpwork } from 'react-icons/si';
 import { images }  from '../constants';
-
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
       <div>
+        <a href='https://www.upwork.com'><SiUpwork /></a>
+        </div>
+        <div>
         <a href='https://wa.me/message/F6YZDMW4H7IDF1'><BsWhatsapp /></a>
         </div>
         <div>
-        <a href='https://telegram.org'><FaTelegram /></a>
+        <a href='https://telegram.me/mnm_2k22'><FaTelegram /></a>
         </div>
         <div>
         <a href='https://www.facebook.com'><FaFacebook /></a>
         </div>
         <div>
-        <a href='https://vk.com'><FaVk /></a>
+        <a href='https://vk.com/id755397428'><FaVk /></a>
         </div>
         <div>
         <a href='https://www.twitter.com/mnm_2k22'><BsTwitter /></a>
         </div>
         <div>
-        <a href='https://weibo.com'><FaWeibo /></a>
+        <a href='https://www.tumblr.com/mnm-team'><FaTumblr /></a>
         </div>
         <div>
-        <a href='https://www.tumblr.com'><FaTumblr /></a>
-        </div>
-        <div>
-        <a href='https://medium.com'><FaMedium /></a>
+        <a href='https://medium.com/@mnm_2k22'><FaMedium /></a>
         </div>
         <div>
         <a href='https://www.instagram.com/mnm_2k22'><BsInstagram /></a>
         </div>
         <div>
-        <a href='https://pinterest.com'><FaPinterestP /></a>
+        <a href='https://www.pinterest.com/mnm2k22'><FaPinterestP /></a>
         </div>
         <div>
         <a href='https://www.youtube.com/channel/UC90sDilVDnaN3idFsIHL0pQ'><FaYoutube /></a>
         </div>
         <div>
-        <a href='https://www.vimeo.com'><FaVimeoV /></a>
+        <a href='https://vimeo.com/channels/1816871'><FaVimeoV /></a>
         </div>
         <div>
-        <a href='https://rumble.com/c/c-1995432'><img src={images.rumble}/></a>
+        <a href='https://rumble.com/c/c-1995432'><img src={images.rumble} alt=''/></a>
         </div>
         <div>
-        <a href='https://www.patreon.com'><FaPatreon /></a>
+        <a href='https://www.patreon.com/user/creators?u=81116952'><FaPatreon /></a>
         </div>
         <div>
         <a href='https://www.linkedin.com/in/mnm-team-788610253'><FaLinkedinIn /></a>
@@ -56,7 +56,7 @@ const SocialMedia = () => {
         <a href='https://discord.gg/kXvyQdAJ'><BsDiscord /></a>
         </div>
         <div>
-        <a href='https://www.spotify.com'><FaSpotify /></a>
+        <a href='https://open.spotify.com/user/31kgc4lhw2wtcsolw64nwueigsaa?si=TRi1AbcNQ_GeU5TZ3hyRTg&utm_source=whatsapp'><FaSpotify /></a>
         </div>
   </div>
   )
