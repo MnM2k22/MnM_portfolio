a40b27ffd2437e8a2c58c625ee8963e54e312432
MnM2k22 <115646994+MnM2k22@users.noreply.github.com>
2022-10-13 12:03:47 +0530

    Update SocialMedia.jsx

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram, BsDiscord } from 'react-icons/bs'
-import { FaFacebook, FaTiktok, FaYoutube, FaLinkedinIn } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsDiscord, BsWhatsapp } from 'react-icons/bs'
+import {  FaTiktok, FaYoutube, FaLinkedinIn } from 'react-icons/fa';
+import { images }  from '../constants';
 
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-        <div>
-        <a href='https://www.twitter.com/mnm_2k22'><BsTwitter /></a>
+       <div>
+        <a href='https://wa.me/message/KMM7DA6TMGDJE1'><BsWhatsapp /></a>
         </div>
         <div>
-        <a href='https://www.facebook.com'><FaFacebook /></a>
+        <a href='https://www.twitter.com/mnm_2k22'><BsTwitter /></a>
         </div>
         <div>
         <a href='https://www.instagram.com/mnm_2k22'><BsInstagram /></a>
@@ -25,9 +26,8 @@ const SocialMedia = () => {
         <a href='https://discord.gg/kXvyQdAJ'><BsDiscord /></a>
         </div>
         <div>
-        <a href='https://www.tiktok.com'><FaTiktok /></a>
+        <a href='https://rumble.com/c/c-1995432'><img src={images.rumble}/></a>
         </div>
-
   </div>
   )
 }
