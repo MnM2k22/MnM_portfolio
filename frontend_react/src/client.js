7e6a263678d119ecaa7d50da971f264defbdf411
MnM2k22 <115646994+MnM2k22@users.noreply.github.com>
2022-10-13 20:48:46 +0530

    Update client.js

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: 'gwu1wusq',
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset:'production',
     apiVersion:'2022-02-01',
     useCdn: true,
-    token: 'skDW1g3MRNtqwbnNLXWPTV4xR6dmUlejNQuyCsCvBUB4NHZdsAPYSp92XY22IRAjC4E7Teo3YkqV5jejYg8exIHU3vow1e5broMNNwmQCYogtytLS1z2MrNyoRcrjlM2eenVbISKIsNDdfEkGzqHQErYBTrMyTWryMwNbPZuhXMS2UTUbxrz',
+    token: process.env.SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
